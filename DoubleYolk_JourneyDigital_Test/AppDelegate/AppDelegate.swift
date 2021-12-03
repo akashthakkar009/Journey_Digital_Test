dc8a26bccb8eecbebb40b552c2fc115d563a89b9
Akash Thakkar <akash@doubleyolk.com>
2021-12-03 18:40:37 +0530

    - Initial project structure setup commit

---
+++ DoubleYolk_JourneyDigital_Test/AppDelegate/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  DoubleYolk_JourneyDigital_Test
+//
+//  Created by DoubleYolk on 03/12/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
